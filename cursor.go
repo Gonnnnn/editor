d964f6a4af18a71af2afdd32ac70899b4d7d5c58
Gon Kim <geonkim23@gmail.com>
2024-03-02 18:51:42 +0900

    Implement a cursor

---
+++ cursor.go
@@ -0,0 +1,78 @@
+package main
+
+import "sync"
+
+type cursor struct {
+	x int
+	y int
+	mu *sync.Mutex
+}
+
+func NewCursor(initX int, initY int) *cursor {
+	return &cursor{
+		x: initX,
+		y: initY,
+		mu: &sync.Mutex{},
+	}
+}
+
+func (c *cursor) MoveUp() (int, int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.y > 0 {
+		c.y--
+	}
+	return c.x, c.y
+}
+
+func (c *cursor) MoveDown(maxHeight int) (int, int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.y < maxHeight-1 {
+		c.y++
+	}
+	return c.x, c.y
+}
+
+func (c *cursor) MoveLeft(maxWidth int) (int, int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.x > 0 {
+		c.x--
+		return c.x, c.y
+	}
+	if c.y > 0 {
+		c.y--
+		c.x = maxWidth - 1
+	}
+	return c.x, c.y
+}
+
+func (c *cursor) MoveRight(maxWidth int, maxHeight int) (int, int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.x < maxWidth-1 {
+		c.x++
+		return c.x, c.y
+	}
+	if c.y < maxHeight-1 {
+		c.y++
+		c.x = 0
+		return c.x, c.y
+	}
+	return c.x, c.y
+}
+
+func (c *cursor) Enter(maxHeight int) (int, int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.y < maxHeight-1 {
+		c.y++
+		c.x = 0
+	}
+	return c.x, c.y
+}
+
+func (c *cursor)CurLocation() (int, int) {
+	return c.x, c.y
+}
--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"editor/golang/array"
 	"fmt"
 
 	"github.com/nsf/termbox-go"
 )
+
+var ARROW_KEYS = []termbox.Key{
+	termbox.KeyArrowUp,
+	termbox.KeyArrowDown,
+	termbox.KeyArrowLeft,
+	termbox.KeyArrowRight,
+}
 
 func main() {
 	err := termbox.Init()
@@ -13,8 +21,8 @@ func main() {
 	}
 	defer termbox.Close()
 
-	var buffer []rune
-	curX, curY := 0, 0
+	cursor := NewCursor(0 /* =initX */, 0 /* =initY */)
+	boxWidth, boxHeight := termbox.Size()
 loop:
 	for {
 		switch ev := termbox.PollEvent(); ev.Type {
@@ -22,23 +30,53 @@ loop:
 			if ev.Key == termbox.KeyEsc {
 				break loop
 			}
-			termbox.SetCell(curX, curY, ev.Ch, termbox.ColorDefault, termbox.ColorDefault)
-			curX++
-
-			boxWidth, _ := termbox.Size()
-			if curX >= boxWidth {
-				curX = 0
-				curY++
+			if array.Contains(ARROW_KEYS, ev.Key) {
+				x, y := handleArrowKey(ev, cursor, boxWidth, boxHeight)
+				termbox.SetCursor(x, y)
+				termbox.Flush()
+				continue
 			}
-			termbox.SetCursor(curX, curY)
+			if ev.Key == termbox.KeyEnter {
+				x, y := cursor.Enter(boxHeight)
+				termbox.SetCursor(x, y)
+				termbox.Flush()
+				continue
+			}
+			if ev.Key == termbox.KeyBackspace || ev.Key == termbox.KeyBackspace2 {
+				curX, curY := cursor.CurLocation()
+				termbox.SetCell(curX, curY, ' ', termbox.ColorDefault, termbox.ColorDefault)
+				x, y := cursor.MoveLeft(boxWidth)
+				termbox.SetCursor(x, y)
+				termbox.Flush()
+				continue
+			}
+		
+			curX, curY := cursor.CurLocation()
+			termbox.SetCell(curX, curY, ev.Ch, termbox.ColorDefault, termbox.ColorDefault)
+			newX, newY := cursor.MoveRight(boxWidth, boxHeight)
+			termbox.SetCursor(newX, newY)
 			termbox.Flush()
 
-			buffer = append(buffer, ev.Ch)
 		case termbox.EventError:
 			fmt.Println(ev.Err)
 			break loop
 		default:
 			fmt.Printf("ev: %+v\n", ev)
 		}
+	}
+}
+
+func handleArrowKey(ev termbox.Event, cursor *cursor, boxWidth int, boxHeight int) (int, int) {
+	switch {
+	case ev.Key == termbox.KeyArrowUp:
+		return cursor.MoveUp()
+	case ev.Key == termbox.KeyArrowDown:
+		return cursor.MoveDown(boxHeight)
+	case ev.Key == termbox.KeyArrowLeft:
+		return cursor.MoveLeft(boxWidth)
+	case ev.Key == termbox.KeyArrowRight:
+		return cursor.MoveRight(boxWidth, boxHeight)
+	default:
+		return cursor.CurLocation()
 	}
 }
